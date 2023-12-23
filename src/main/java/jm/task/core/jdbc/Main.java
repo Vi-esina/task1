72111e1502b2ae6215c6ffd70280b4834b41aa26
МVi-esina <viktorija.esman@gmail.com>
2023-12-23 13:31:29 +0300

    jdbc, hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
        UserService userDao= new UserServiceImpl();
+       userDao.dropUsersTable();
         userDao.createUsersTable();
         userDao.saveUser("Max", "Sokolov", (byte) 33);
         userDao.saveUser("Egor", "Drozdov", (byte) 12);
@@ -25,6 +26,7 @@ public class Main {
         }
 userDao.cleanUsersTable();
         userDao.dropUsersTable();
+
     }
 
 }
